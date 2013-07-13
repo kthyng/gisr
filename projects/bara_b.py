31eb220d1d08f72d346f078c49f4f46a1b2fe42b
Kristen M. Thyng <kthyng@gmail.com>
2013-07-13 14:00:31 -0500

    Fixed bara_b.py

--- projects/bara_b.py
+++ projects/bara_b.py
@@ -1,6 +1,6 @@
 '''
-Script to run the backward Galveston drifter simulation. Start the 
-drifters near Galveston and run them backward.
+Script to run the backward Barataria drifter simulation. Start the 
+drifters near Barataria and run them backward.
 '''
 
 import numpy as np
@@ -18,12 +18,12 @@ def run():
     # Make sure necessary directories exist
     if not os.path.exists('tracks'):
         os.makedirs('tracks')
-    if not os.path.exists('tracks/galv_b'):
-        os.makedirs('tracks/galv_b')
+    if not os.path.exists('tracks/bara_b'):
+        os.makedirs('tracks/bara_b')
     if not os.path.exists('figures'):
         os.makedirs('figures')
-    if not os.path.exists('figures/galv_b'):
-        os.makedirs('figures/galv_b')
+    if not os.path.exists('figures/bara_b'):
+        os.makedirs('figures/bara_b')
 
     # Parameters to be rotated through
     years = np.array([2010])
@@ -33,7 +33,7 @@ def run():
     ndays = 87 # to cover oil spill time period
 
     # Do one initialization here to save grid
-    _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, grid = init.galv_b()
+    _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, grid = init.bara_b()
 
     # loop through start dates for drifters
     for n in xrange(ndays):
@@ -43,7 +43,7 @@ def run():
 
         # Read in simulation initialization
         loc, nstep, ndays, ff, date, tseas, ah, av, lon0, lat0, z0, \
-                zpar, do3d, doturb, name, grid = init.galv_b(date, grid=grid)
+                zpar, do3d, doturb, name, grid = init.bara_b(date, grid=grid)
 
         # If the particle trajectories have not been run, run them
         if not os.path.exists('tracks/' + name + '.nc'):
@@ -64,45 +64,3 @@ def run():
         if not os.path.exists('figures/' + name + 'histhexbin.png'):
             tracpy.plotting.hist(lonp, latp, name, grid=grid, \
                                 which='hexbin', bins=(40,40))
-
-    #     # Save up tracks to plot together by month
-    #     if n == 0:
-    #         lonpsavem = lonp
-    #         latpsavem = latp
-    #     else:
-    #         lonpsavem = np.vstack((lonpsavem,lonp))
-    #         latpsavem = np.vstack((latpsavem,latp))
-    #     if n == 0: # initially, give month a value
-    #         month = date.month
-    #     # want to know when we are at a new month or the end of the year
-    #     elif (date.month != month) or \
-    #             (date.month == 12 and date.day == 31): 
-    #         # First plot previous month
-    #         # take off day and month and add on previous month instead
-    #         name = name[:-5] + str(month).zfill(2)
-    #         tracpy.plotting.tracks(lonpsavem, latpsavem, name, grid=grid)
-    #         tracpy.plotting.hist(lonpsavem, latpsavem, name, grid=grid, \
-    #                             which='hexbin', bins=(40,40))
-    #         # Reset month to next month value
-    #         month = date.month
-    #         # Save month arrays for year plot
-    #         if month == 1:
-    #             lonpsavey = lonp
-    #             latpsavey = latp
-    #         else:
-    #             lonpsavey = np.vstack((lonpsavey,lonpsavem))
-    #             latpsavey = np.vstack((latpsavey,latpsavem))
-    #         # Reset save arrays for month plots
-    #         lonpsavem = lonp
-    #         latpsavem = latp
-
-    # # Plot year summaries
-    # name = str(year)
-    # tracpy.plotting.tracks(lonpsavey, latpsavey, name, grid=grid)
-    # tracpy.plotting.hist(lonpsavey, latpsavey, name, grid=grid, \
-    #                     which='hexbin', bins=(40,40))
-
-    # ## Weatherband plotting
-    # # Read in all tracks
-    # files = np.sort(glob.glob('tracks/galv_b/*.nc')) # sorted list of file names
-    
