a3f9d92e7db1de0e45a32042ad6008ece4d458e2
Kristen M. Thyng <kthyng@gmail.com>
2013-08-05 15:18:32 -0500

    altering logic of dwh_stream_f for convenience

--- projects/dwh_stream_f.py
+++ projects/dwh_stream_f.py
@@ -41,45 +41,45 @@ def run():
 
     rundays = 87 # to cover oil spill time period
 
-    # Do one initialization here to save grid
-    _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, grid = init.dwh_f()
+    # # Do one initialization here to save grid
+    # _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, grid = init.dwh_f()
 
-    # loop through numbers of drifters for test
-    for N in Ns:
+    # # loop through numbers of drifters for test
+    # for N in Ns:
 
-        # loop through start dates for drifters
-        for n in xrange(rundays):
+    #     # loop through start dates for drifters
+    #     for n in xrange(rundays):
 
-            # Run for each output during the day too
-            for nh in range(0,24,4):
+    #         # Run for each output during the day too
+    #         for nh in range(0,24,4):
 
-                # Date for this loop
-                date = startdate + timedelta(days=n) + timedelta(hours=nh)
+    #             # Date for this loop
+    #             date = startdate + timedelta(days=n) + timedelta(hours=nh)
 
-                # Read in simulation initialization
-                loc, nstep, ndays, ff, date, tseas, ah, av, \
-                        lon0, lat0, z0, zpar, do3d, doturb, \
-                        name, grid, dostream, T0, \
-                        U, V = init.dwh_stream_f(date, N, grid=grid)
+    #             # Read in simulation initialization
+    #             loc, nstep, ndays, ff, date, tseas, ah, av, \
+    #                     lon0, lat0, z0, zpar, do3d, doturb, \
+    #                     name, grid, dostream, T0, \
+    #                     U, V = init.dwh_stream_f(date, N, grid=grid)
 
-                # If the particle trajectories have not been run, run them
-                if not os.path.exists('tracks/' + name + '.nc'):
-                    lonp, latp, zp, t, grid, T0, U, V = tracpy.run.run(loc, nstep, ndays, \
-                                                    ff, date, tseas, ah, av, \
-                                                    lon0, lat0, z0, zpar, do3d, \
-                                                    doturb, name, grid=grid, \
-                                                    dostream=dostream, T0=T0, \
-                                                    U=U, V=V)
-                elif not os.path.exists('figures/' + name + 'tracks.png') or \
-                     not os.path.exists('figures/' + name + 'histhexbin.png'):
-                    d = netCDF.Dataset('tracks/' + name + '.nc')
-                    lonp = d.variables['lonp'][:]
-                    latp = d.variables['latp'][:]
-                    T0 = d.variables['T0'][:]
-                    U = d.variables['U'][:]
-                    V = d.variables['V'][:]
-                    tracpy.plotting.tracks(lonp, latp, name, grid=grid)
-                    tracpy.plotting.hist(lonp, latp, name, grid=grid, which='hexbin')
+    #             # If the particle trajectories have not been run, run them
+    #             if not os.path.exists('tracks/' + name + '.nc'):
+    #                 lonp, latp, zp, t, grid, T0, U, V = tracpy.run.run(loc, nstep, ndays, \
+    #                                                 ff, date, tseas, ah, av, \
+    #                                                 lon0, lat0, z0, zpar, do3d, \
+    #                                                 doturb, name, grid=grid, \
+    #                                                 dostream=dostream, T0=T0, \
+    #                                                 U=U, V=V)
+    #             elif not os.path.exists('figures/' + name + 'tracks.png') or \
+    #                  not os.path.exists('figures/' + name + 'histhexbin.png'):
+    #                 d = netCDF.Dataset('tracks/' + name + '.nc')
+    #                 lonp = d.variables['lonp'][:]
+    #                 latp = d.variables['latp'][:]
+    #                 T0 = d.variables['T0'][:]
+    #                 U = d.variables['U'][:]
+    #                 V = d.variables['V'][:]
+    #                 tracpy.plotting.tracks(lonp, latp, name, grid=grid)
+    #                 tracpy.plotting.hist(lonp, latp, name, grid=grid, which='hexbin')
     
     # Do transport plot
     tracpy.plotting.transport(name='dwh_stream_f', Title='Deepwater Horizon Spill Transport',
