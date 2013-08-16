61a51ffba81fd074638e0f49fa670cb47915d1de
Kristen M. Thyng <kthyng@gmail.com>
2013-08-16 18:21:11 -0500

    fix to galvcon_b_plot

--- projects/galvcon_b_plot.py
+++ projects/galvcon_b_plot.py
@@ -15,9 +15,9 @@ Files = glob.glob('tracks/galvcon_b/*.nc')
 Files.sort()
 
 # number of days to look at
-ndays = 20
+ndays = 10
 # d drifters for quiver
-dd = 2
+dd = 1
 
 # Choose a particular wind location
 iind= 330; jind = 90;
