96a66531d97a779f7a26a7a9a86698175c42b1b1
Kristen M. Thyng <kthyng@gmail.com>
2013-08-16 18:15:52 -0500

    fix to galvcon_b_plot

--- projects/galvcon_b_plot.py
+++ projects/galvcon_b_plot.py
@@ -11,67 +11,13 @@ from datetime import datetime, timedelta
 import glob
 from matplotlib.mlab import find
 
-def smooth(x, window_len=10, window='hanning'):
-    """smooth the data using a window with requested size.
-    
-    This method is based on the convolution of a scaled window with the signal.
-    The signal is prepared by introducing reflected copies of the signal 
-    (with the window size) in both ends so that transient parts are minimized
-    in the begining and end part of the output signal.
-    
-    input:
-        x: the input signal 
-        window_len: the dimension of the smoothing window
-        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
-            flat window will produce a moving average smoothing.
-
-    output:
-        the smoothed signal
-        
-    example:
-
-    import numpy as np    
-    t = np.linspace(-2,2,0.1)
-    x = np.sin(t)+np.random.randn(len(t))*0.1
-    y = smooth(x)
-    
-    see also: 
-    
-    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
-    scipy.signal.lfilter
- 
-    TODO: the window parameter could be the window itself if an array instead of a string   
-    """
-
-    if x.ndim != 1:
-        raise ValueError, "smooth only accepts 1 dimension arrays."
-
-    if x.size < window_len:
-        raise ValueError, "Input vector needs to be bigger than window size."
-
-    if window_len < 3:
-        return x
-
-    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
-        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
-
-    s=np.r_[2*x[0]-x[window_len:1:-1], x, 2*x[-1]-x[-1:-window_len:-1]]
-    #print(len(s))
-    
-    if window == 'flat': #moving average
-        w = np.ones(window_len,'d')
-    else:
-        w = getattr(np, window)(window_len)
-    y = np.convolve(w/w.sum(), s, mode='same')
-    return y[window_len-1:-window_len+1]
-
 Files = glob.glob('tracks/galvcon_b/*.nc')
 Files.sort()
 
 # number of days to look at
 ndays = 60
 # d drifters for quiver
-dd = 6
+dd = 3
 
 # Choose a particular wind location
 iind= 330; jind = 90;
@@ -110,8 +56,8 @@ for File in Files:
 
 	# Negative sign since backward in time
 	# also smooth
-	wx = -smooth(wx, window_len=dd/2)
-	wy = -smooth(wy, window_len=dd/2)
+	wx = -wx
+	wy = -wy
 
 	# # Average model output
 	# wxm = np.mean(wx,0)
